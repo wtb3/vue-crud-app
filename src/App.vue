257d429ffe9d1ea737c3cfe4004d24de065001fe
Warren Barnes <warrenthomasbarnes@gmail.com>
2023-08-22 13:27:54 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table :employees="employees" />
+    <employee-form @add:employee="addEmployee" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com",
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId = this.employees.length > 0 ? this.employees[this.employees.length - 1].id : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,39 @@
+<!-- src/components/EmployeeForm.vue -->
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-form",
+  data() {
+    return {
+      employee: {
+        name: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("add:employee", this.employee);
+      this.employee = {
+        name: "",
+        email: "",
+      };
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
